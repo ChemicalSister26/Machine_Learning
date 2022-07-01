e971ed1ea1b647e86b9082a83b9c51d4c2424bc5
alberdinamariya <mariyaalberdina1980@gmail.com>
2022-07-01 10:06:47 +0300

    commit 1 july/1

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 import torch
+import numpy as np
+import math
 
-torch.Tensor([0,1,1])
+data = [[1, 2], [3, 4]]
+a = torch.tensor(data)
+np = np.array(data)
+print(np)
+
+b = torch.from_numpy(np)
+print(b)
+
+c = torch.ones_like(a)
+print(c)
+d = torch.rand_like(a, dtype=torch.float)
+print(d)
+
+shape = (2, 3,)
+
+s = torch.rand(shape)
+e = torch.ones(shape)
+f = torch.zeros(shape)
+
+print(s, e, f)
