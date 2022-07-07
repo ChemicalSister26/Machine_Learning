599805c391eba0c621b965e6127b26b39dcbe9b4
alberdinamariya <mariyaalberdina1980@gmail.com>
2022-07-07 14:58:21 +0300

    commit 7 july

--- main.py
+++ main.py
@@ -6,100 +6,57 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets, transforms
 from torchvision.transforms import ToTensor, Lambda
 import matplotlib.pyplot as plt
-
-# import numpy as np
-# X = np.array([1, 2, 3, 4], dtype=np.float32)
-# Y = np.array([2, 4, 6, 8], dtype=np.float32)
-#
-# w = 0.0
-# #forward pass
-# def forward(x):
-#     return w*x
-#
-# #loss
-# def loss(y, y_pred):
-#   return(((y_pred-y)**2).mean())
-#
-# #gradient
-# def grad(x,y,y_pred):
-#     return np.dot(2*x, y_pred-y).mean()
-#
-# print(f'Prediction before training {forward(5)}')
-#
-# #training
-#
-# learning_rate = 0.01
-# epoch_number = 20
-#
-# for epoch in range(epoch_number):
-#     #forward calculations
-#     y_prediction = forward(X)
-#     #loss
-#     l = loss(Y, y_prediction)
-#
-#     #gradients
-#     dw = grad(X, Y, y_prediction)
-#
-#     #update weights
-#     w -= learning_rate*dw
-#
-#     if epoch % 1 == 0:
-#         print(f'weight {w:3f}, loss {l:.8f}')
-#
-# print(f'Prediction after training {forward(5)}')
+from sklearn import datasets
+import numpy as np
 
 
-X = torch.tensor([[1], [2], [3], [4]], dtype=torch.float32)
-Y = torch.tensor([[2], [4], [6], [8]], dtype=torch.float32)
+#preparing data
+X_numpy, y_numpy = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=1)
 
-X_test = torch.tensor([5], dtype=torch.float32)
+x = torch.from_numpy(X_numpy.astype(np.float32))
+y = torch.from_numpy(y_numpy.astype(np.float32))
 
-number_samples, number_features = X.shape
+y = y.view(y.shape[0], 1)
+
+number_samples, number_features = x.shape
+
+# create model
+
 input_size = number_features
-output_size = number_features
+output_size = 1
 
+model = nn.Linear(input_size, output_size)
 
-
-class LinearRegression(nn.Module):
-    def __init__(self, input_dim, output_dim):
-        super(LinearRegression, self).__init__()
-
-    #define layers
-        self.lin = nn.Linear(input_dim, output_dim)
-
-    def forward(self, x):
-        return self.lin(x)
-
-model = LinearRegression(input_size, output_size)
-
-
-print(f'Prediction before training {model(X_test).item()}')
-
-#training
-
+# loss and optimizer
 learning_rate = 0.01
-epoch_number = 1000
-
-loss = nn.MSELoss()
+criteria = nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
+#traininhg loop
+
+epoch_number = 100
 for epoch in range(epoch_number):
     #forward calculations
-    y_prediction = model(X)
+    y_prediction = model(x)
     #loss
-    l = loss(Y, y_prediction)
+    loss = criteria(y_prediction, y)
 
     #gradients = backward pass dl/dw
-    l.backward()
+    loss.backward()
 
     #update weights
     optimizer.step()
 
-    # zero gradients
+     # zero gradients
     optimizer.zero_grad()
 
-    if epoch % 10 == 0:
-        [w, b] = model.parameters()
-        print(f'weight {w[0][0].item():3f}, loss {l:.8f}')
 
-print(f'Prediction after training {model(X_test).item()}')
+if epoch % 10 == 0:
+    [w, b] = model.parameters()
+    print(f'loss {loss.item():.8f}')
+
+predicted = model(x).detach().numpy()
+plt.plot(x, y, 'ro')
+plt.plot(x, predicted, 'g')
+plt.show()
+
